1b87b21d20b238a996af9ea6d54a6df6c08fac11
santiagocasti <santiago.castineira@gmail.com>
2016-07-29 20:41:37 +0200

    Service to handle the conversion of the app specific parameters to the API specific parameters and retrieving the specific search results.

---
+++ src/main/java/com/fly/me/services/FlightSearchService.java
@@ -0,0 +1,54 @@
+package com.fly.me.services;
+
+import com.fly.me.dtos.pojos.FlightSearchParameters;
+import com.fly.me.dtos.pojos.SearchResponse;
+import com.fly.me.dtos.pojos.qpx.QPXSearchParameters;
+import com.fly.me.dtos.pojos.qpx.QPXSliceParameter;
+import com.fly.me.services.qpx.GoogleQPXAPIService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.format.DateTimeFormatter;
+
+@Service
+public class FlightSearchService {
+
+    @Autowired
+    protected GoogleQPXAPIService googleQPXAPIService;
+
+    @Autowired
+    protected SearchResultsService searchResultsService;
+
+    public int findFlights(FlightSearchParameters parameters) {
+        QPXSearchParameters params = convertToQPXFormat(parameters);
+        SearchResponse response = googleQPXAPIService.searchFlights(params);
+        return searchResultsService.saveSearchResults(response);
+    }
+
+    protected QPXSearchParameters convertToQPXFormat(FlightSearchParameters parameters) {
+
+        QPXSearchParameters params = new QPXSearchParameters();
+        params.setAdultCount(parameters.getAdultCount());
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyy-MM-dd");
+
+        QPXSliceParameter slice = new QPXSliceParameter();
+        slice.setOrigin(parameters.getOrigin());
+        slice.setDestination(parameters.getDestination());
+        slice.setDate(parameters.getFlightOutDate().format(formatter));
+        params.addSlice(slice);
+
+        if (parameters.isReturnFlight()) {
+            slice = new QPXSliceParameter();
+            slice.setOrigin(parameters.getDestination());
+            slice.setDestination(parameters.getOrigin());
+            slice.setDate(parameters.getFlightBackDate().format(formatter));
+            params.addSlice(slice);
+        }
+
+        return params;
+    }
+
+
+
+}
