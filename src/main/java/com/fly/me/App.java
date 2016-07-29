0840065a76de16f53bb7f5e363fe5b961baff90a
santiagocasti <santiago.castineira@gmail.com>
2016-07-29 20:37:17 +0200

    Adding support to deploy in a standalone servlet container.

--- src/main/java/com/fly/me/App.java
+++ src/main/java/com/fly/me/App.java
@@ -2,6 +2,8 @@ package com.fly.me;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -19,13 +21,19 @@ import java.util.logging.Logger;
 @ComponentScan
 @EnableAutoConfiguration
 @EnableScheduling
-public class App {
+public class App extends SpringBootServletInitializer{
 
     private static final Logger logger = Logger.getLogger(App.class.toString());
 
+    // Used when launching as an executable jar or war
     public static void main(String[] args) {
         ApplicationContext ctx = SpringApplication.run(App.class, args);
+        logger.info("Starting app from executing a jar/war...");
+    }
 
-        logger.info("[s] Starting app...");
+    // used when deploying to a standalone servlet container
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        logger.info("Starting app from standalone servlet container");
+        return applicationBuilder.sources(App.class);
     }
 }
