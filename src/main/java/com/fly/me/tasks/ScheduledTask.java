bf2a4e65523d9257b76c546435b9683904d95e5e
santiagocasti <santiago.castineira@gmail.com>
2016-08-07 17:01:07 +0200

    Optimizing imports.

--- src/main/java/com/fly/me/tasks/ScheduledTask.java
+++ src/main/java/com/fly/me/tasks/ScheduledTask.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.Month;
 import java.util.Date;
